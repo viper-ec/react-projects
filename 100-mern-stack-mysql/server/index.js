6a389f375b93db9c18a67064482a897156162449
Victor Viscarra <vviscarra.dce@gmail.com>
2022-11-11 00:52:10 -0500

    Implementa cambios para despliegue de app

--- 100-mern-stack-mysql/server/index.js
+++ 100-mern-stack-mysql/server/index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import { PORT } from './config.js'
+
+// Routes
 import indexRoutes from './routes/index.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
 
 // Modules
 app.use(
@@ -18,6 +24,8 @@ app.use(express.json())
 // Routes
 app.use(indexRoutes)
 app.use(taskRoutes)
+
+app.use(express.static(join(__dirname, '../client/dist')))
 
 // Listen
 app.listen(PORT)
